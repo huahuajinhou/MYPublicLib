1b1bc1ba3f19091a5c19d2700d58fb1251372bd7
apple <apple@appledeMac-mini.local>
2019-01-02 11:30:38 +0800

    1st

---
+++ Classes/Const.h
@@ -0,0 +1,13 @@
+//
+//  Const.h
+//  TestPublicLibProject
+//
+//  Created by apple on 2019/1/2.
+//  Copyright © 2019 MY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Const : NSObject
+- (void)eat;
+@end
---
+++ Classes/Const.m
@@ -0,0 +1,15 @@
+//
+//  Const.m
+//  TestPublicLibProject
+//
+//  Created by apple on 2019/1/2.
+//  Copyright © 2019 MY. All rights reserved.
+//
+
+#import "Const.h"
+
+@implementation Const
+- (void)eat {
+    NSLog(@"尽情的吃");
+}
+@end
